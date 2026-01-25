bc31ad8e5c71f9088eda3a96df51d00c747e367a
afshan <mdafshan0401@gmail.com>
2026-01-25 19:32:37 +0530

    changes made

--- Searching_algos/BinarySearchVariations/KokoEatingBananas.java
+++ Searching_algos/BinarySearchVariations/KokoEatingBananas.java
@@ -12,28 +12,29 @@ public class KokoEatingBananas {
     public static int calcBananasPerHour(int[] piles, int h) {
         int low = 1;
         int high = 0;
-        for(int p : piles){
+        for (int p : piles) {
             high = Math.max(p, high);
         }
         int k = high;
 
-        while(low <= high){
-            int mid = low + (high-low)/2;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
             long hours = 0;
-            for(int p: piles){
-                hours += (p + mid -1) / mid;
-                if (hours>h) break;
+            for (int p : piles) {
+                hours += (p + mid - 1) / mid;
+                if (hours > h)
+                    break;
             }
 
-            if(hours<=h){
+            if (hours <= h) {
                 k = mid;
                 high = mid - 1;
-            } else 
+            } else
                 low = mid + 1;
         }
 
         return k;
-        
+
     }
 
     public static int bruteApproach(int[] piles, int h) {
@@ -46,7 +47,7 @@ public class KokoEatingBananas {
             int hours = 0;
 
             for (int pile : piles) {
-                // ceil(a / b) = (a + b - 1) / b  always mathematically true
+                // ceil(a / b) = (a + b - 1) / b always mathematically true
                 hours += (pile + k - 1) / k;
                 // hours += (int) Math.ceil((double) pile / k); this can be used too
             }
