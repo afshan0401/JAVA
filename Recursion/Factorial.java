a5f24429e4a213daf02586837d836187f26e30d2
afshan <mdafshan0401@gmail.com>
2026-02-15 16:28:38 +0530

    changes made

--- Recursion/Factorial.java
+++ Recursion/Factorial.java
@@ -3,7 +3,7 @@ public class Factorial {
     public static void main(String[] args) {
         Factorial t1 = new Factorial();
 
-        long n = 20;
+        long n = 5;
         long prod = 1;
         for (int i = 2; i <= n; i++) {
             prod *= i;
@@ -19,7 +19,7 @@ public class Factorial {
     
     public long recurFactorial(long n){
         if(n<0){
-            throw new IllegalArgumentException("Invalid inupt");
+            throw new IllegalArgumentException("Invalid input");
         }
         if (n==0 || n ==1 ) return 1;
 
