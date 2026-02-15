f43680b38d5920f5457700525ba9a3b833693eb0
afshan <mdafshan0401@gmail.com>
2026-02-15 16:05:43 +0530

    factorial

---
+++ Recursion/Factorial.java
@@ -0,0 +1,28 @@
+
+public class Factorial {
+    public static void main(String[] args) {
+        Factorial t1 = new Factorial();
+
+        long n = 20;
+        long prod = 1;
+        for (int i = 2; i <= n; i++) {
+            prod *= i;
+        }
+        System.out.println(prod);
+
+
+        long ans = t1.recurFactorial(n);
+        System.out.println(ans);
+
+        
+    }
+    
+    public long recurFactorial(long n){
+        if(n<0){
+            throw new IllegalArgumentException("Invalid inupt");
+        }
+        if (n==0 || n ==1 ) return 1;
+
+        return n * recurFactorial(n-1);
+    }
+}
